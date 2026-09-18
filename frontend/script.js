486c05bbd64154d8b7eec1aa1ac69a51c1ad84a9
Aluno <e023aluno@outlook.com>
2026-09-18 16:51:09 -0300

    Exibe titulo, autor e ano dos livros na tela

--- frontend/script.js
+++ frontend/script.js
@@ -33,8 +33,22 @@ formulario.addEventListener('submit', async (evento) => {
     }
 
     mensagem.textContent = dados.total + ' resultado(s) encontrado(s)';
-    console.log(dados.livros);
+    mostrarLivros(dados.livros);
   } catch (erro) {
     mensagem.textContent = 'Nao foi possivel conectar ao servidor';
   }
+
+  function mostrarLivros(livros) {
+  livros.forEach((livro) => {
+    const div = document.createElement('div');
+    div.className = 'livro';
+
+    div.innerHTML =
+      '<h3>' + livro.titulo + '</h3>' +
+      '<p>Autor: ' + livro.autor + '</p>' +
+      '<p>Ano: ' + livro.ano + '</p>';
+
+    resultados.appendChild(div);
+  });
+}
 });
